54dddd737bbde28bdd56bbc260cef00d5b240f64
priyanahata <p@anahata.uno>
2026-03-25 22:28:23 +0100

    javadoc

---
+++ anahata-asi-nb/src/main/java/uno/anahata/asi/nb/module/package-info.java
@@ -0,0 +1,13 @@
+/* Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça! */
+/**
+ * Provides utilities for interacting with the NetBeans module system and 
+ * lifecycle.
+ * <p>
+ * This package contains low-level utilities for introspecting the module 
+ * graph, calculating classpaths for dynamic compilation, and managing 
+ * module-specific metadata.
+ * </p>
+ * 
+ * @author anahata
+ */
+package uno.anahata.asi.nb.module;
