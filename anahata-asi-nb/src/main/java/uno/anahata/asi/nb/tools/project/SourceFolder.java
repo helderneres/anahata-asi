378e66390fa9900f26b5533442d190648d050d88
priyanahata <p@anahata.uno>
2026-03-27 11:58:08 +0100

    javadoc

--- anahata-asi-nb/src/main/java/uno/anahata/asi/nb/tools/project/SourceFolder.java
+++ anahata-asi-nb/src/main/java/uno/anahata/asi/nb/tools/project/SourceFolder.java
@@ -1,4 +1,4 @@
-/* Licensed under the Apache License, Version 2.0 */
+/* Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça! */
 package uno.anahata.asi.nb.tools.project;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -18,19 +18,24 @@ import lombok.Data;
 @AllArgsConstructor
 public final class SourceFolder {
     
+    /** The display name of the source folder, if it differs from the physical folder name. */
     @Schema(description = "The display name of the source folder (e.g., 'Source Packages'). This is only included if it differs from the actual folder name.")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private final String displayName;
 
+    /** The absolute physical path to the folder on the host filesystem. */
     @Schema(description = "The absolute path to the folder.")
     private final String path;
 
+    /** The total recursive size in bytes of all contents within this folder. */
     @Schema(description = "The total size of all files within this folder and all its subfolders, calculated recursively.")
     private final long recursiveSize;
 
+    /** The list of files contained directly within this directory. */
     @Schema(description = "A list of files contained directly within this folder.")
     private final List<ProjectFile> files;
 
+    /** The list of immediate sub-directories. */
     @Schema(description = "A list of subfolders contained directly within this folder.")
     private final List<SourceFolder> subfolders;
 }
